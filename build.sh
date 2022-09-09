13f10db5b654b4af7799b6f4482eae1be5cd9bd0
chirag-parmar <cparmar@blockchains.com>
2022-09-09 12:28:40 +0200

    add build scripts and ci config

---
+++ build.sh
@@ -0,0 +1,12 @@
+# create directory for storing the built static library
+mkdir lib
+
+# enter the vade-evan repository
+cd vade-evan
+
+# build vade-evan
+cargo build --release --no-default-features --features=sdk --target-dir ../lib
+
+# take the static library out and delete everything else
+cp lib/release/libvade_evan.a ./
+rm -rf lib
