83130b006bd971c452c37d1ca2658031960208d1
chirag-parmar <cparmar@blockchains.com>
2022-10-28 12:31:11 +0200

    aarch64-apple-ios

--- build.sh
+++ build.sh
@@ -23,6 +23,31 @@ elif [ $1 = "x86_64-apple-darwin" ] && [[ $target == "x86_64-apple-darwin"* ]]
 then
   # build vade-evan for target using cross
   cargo build --release --no-default-features --features=sdk --target-dir ../build --target $1
+
+elif [ $1 = "armv7s-apple-ios" ] || [ $1 = "armv7-apple-ios" ] || [ $1 = "i686-apple-ios" ] || [ $1 = "x86_64-apple-ios" ]
+then
+  echo "Target build not supported"
+elif [ $1 = "aarch64-apple-ios" ]
+then
+  # replace cargo.toml with a new one omitting compilation of didcomm
+  mv Cargo.toml Cargo.toml.copy
+  cp ../Cargo.toml.new Cargo.toml
+
+  # add target
+  rustup target add $1
+
+  # copy the cross configuration inside vade-evan
+  cp ../Cross.toml Cross.toml
+
+  # build vade-evan for target using cross
+  cross build --release --no-default-features --features=sdk --target-dir ../build --target $1
+
+  # delete cross configuration
+  rm Cross.toml
+
+  # fall back to the original cargo.toml
+  rm Cargo.toml
+  mv Cargo.toml.copy Cargo.toml
 else
   # copy the cross configuration inside vade-evan
   cp ../Cross.toml Cross.toml
