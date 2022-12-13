5628677ceaf1eae246877bdca863b1f7c68b1794
Chirag Mahaveer Parmar <cparmar@blockchains.com>
2022-12-13 10:58:36 +0000

    merge new version of vade

--- build.sh
+++ build.sh
@@ -12,29 +12,27 @@ mkdir build
 # enter the vade-evan repository
 cd vade-evan
 
-# load the target machine of gcc
-target=`gcc -dumpmachine`
+# rustup add architecture
+rustup target add $1
 
-# if target is x86_64-apple-darwin then build locally otherwise build using cross
-if [ $1 = "x86_64-apple-darwin" ] && [[ $target != "x86_64-apple-darwin"* ]]
+# load the target machine of gcc
+host=`gcc -dumpmachine`
+
+if [ $1 = "x86_64-apple-darwin" ] && [[ $host != "x86_64-apple-darwin"* ]]
 then
   echo "Build target is x86_64-apple-darwin which can only be built locally on a mac machine"
-elif [ $1 = "x86_64-apple-darwin" ] && [[ $target == "x86_64-apple-darwin"* ]]
+elif [ $1 = "x86_64-apple-darwin" ] && [[ $host == "x86_64-apple-darwin"* ]]
 then
   # build vade-evan for target using cross
   cargo build --release --no-default-features --features=sdk --target-dir ../build --target $1
-
-elif [ $1 = "armv7s-apple-ios" ] || [ $1 = "armv7-apple-ios" ] || [ $1 = "i686-apple-ios" ] || [ $1 = "x86_64-apple-ios" ]
+elif [ $1 = "armv7s-apple-ios" ] || [ $1 = "armv7-apple-ios" ] || [ $1 = "i686-apple-ios" ] 
 then
   echo "Target build not supported"
-elif [ $1 = "aarch64-apple-ios" ]
+elif [ $1 = "aarch64-apple-ios" ] || [ $1 = "aarch64-apple-ios-sim" ] || [ $1 = "x86_64-apple-ios" ]
 then
   # replace cargo.toml with a new one omitting compilation of didcomm
   mv Cargo.toml Cargo.toml.copy
   cp ../Cargo.toml.new Cargo.toml
-
-  # add target
-  rustup target add $1
 
   # copy the cross configuration inside vade-evan
   cp ../Cross.toml Cross.toml
